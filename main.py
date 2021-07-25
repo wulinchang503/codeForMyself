c9304e4ebcc49ae3483c15a0b6c5d3dc82835bb1
wulinchang1990 <wulinchang503@qq.com>
2021-07-25 10:44:51 +0800

    branchtest

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print("thanks very much")
+    print("hot-fix")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
